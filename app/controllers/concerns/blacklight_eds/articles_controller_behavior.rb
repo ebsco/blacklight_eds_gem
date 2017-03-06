5513d6c62be31cd6db9f930d69d80593cc1a079b
Tadas Paegle <tpaegle@iu.edu>
2017-03-06 10:55:50 -0500

    LDL-1536: Error given when you change articles "view 10 results" to "view 100" / Session Full

--- app/controllers/concerns/blacklight_eds/articles_controller_behavior.rb
+++ app/controllers/concerns/blacklight_eds/articles_controller_behavior.rb
@@ -290,6 +290,9 @@ module BlacklightEds::ArticlesControllerBehavior
   end
 
   def update_results_in_session results
+    #prevent :info from overloading the session by removing it
+    session[:eds].delete :info
+
     eds_session[:results] = results.fetch('SearchResult', {}).fetch('Data', {}).fetch('Records', []).map { |r|
       [r.fetch('Header', {}).fetch('An', nil), r.fetch('Header', {}).fetch('DbId', nil)]
     }
