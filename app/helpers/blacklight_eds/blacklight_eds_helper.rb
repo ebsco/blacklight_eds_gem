c5331c3e28361a1aa0aeadd5632fe5c03b3caab3
jiaola <djiao@jhu.edu>
2019-06-16 15:14:43 -0400

    Implemented IP based authentication

--- app/helpers/blacklight_eds/blacklight_eds_helper.rb
+++ app/helpers/blacklight_eds/blacklight_eds_helper.rb
@@ -803,6 +803,18 @@ module BlacklightEds::BlacklightEdsHelper
     user_signed_in?
   end
 
+  def ip_in_range? ip
+    profiles = Rails.application.config.eds_profiles
+    profile = 'default' # assumes only one campues. TODO: Fix this
+    eds_profile = profiles.fetch(profile, profiles.values[0])
+    eds_profile.fetch('ip_ranges', []).each do |range|
+      if range.include? ip
+        return true
+      end
+    end
+    return false
+  end
+
   def eds_current_user
     current_user
   end
